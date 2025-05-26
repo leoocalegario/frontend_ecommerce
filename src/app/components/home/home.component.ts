c33b71499f81b9ba8f0fad4f99b8b8d6ff81dcaf
Vinicius Casseb <vinicasseb99@gmail.com>
2025-05-26 19:40:48 -0300

    new frontend and things

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,7 @@ import { Carro } from '../../models/carro';
 import { CarroService } from '../../services/carros.service';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -15,43 +16,53 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-    pesquisa: string ="";
+    pesquisa: string = "";
     lista: Carro[] = [];
+    private searchSubject = new Subject<string>();
 
     carrosService = inject(CarroService);
 
-  constructor(){
-    this.listAll();
-  }
-
-    pesquisar(){
-      this.findByModeloLike(this.pesquisa);
+    constructor() {
+        this.listAll();
+        
+        // Configurar o debounce para a busca
+        this.searchSubject.pipe(
+            debounceTime(1), // Espera 1ms após o usuário parar de digitar
+            distinctUntilChanged() // Só executa se o valor mudou
+        ).subscribe(termo => {
+            if (termo.trim() === '') {
+                this.listAll();
+            } else {
+                this.findByModeloLike(termo);
+            }
+        });
     }
 
-
-    listAll(){
-      this.carrosService.listAll().subscribe({
-        next: lista => {
-          this.lista = lista;
-        },
-        error: erro =>{
-          alert('deu erro');
-        }
-      });
+    pesquisar() {
+        this.searchSubject.next(this.pesquisa);
     }
 
-    findByModeloLike(pesquisa: string){
-      console.log(pesquisa);
-      this.carrosService.findByModeloLike(pesquisa).subscribe({
-        next: lista => {
-          console.log(lista);
-          this.lista = lista;
-        },
-        error: erro =>{
-          alert('Ocorreu um erro imprevisto.');
-        }
-      });
+    listAll() {
+        this.carrosService.listAll().subscribe({
+            next: lista => {
+                this.lista = lista;
+            },
+            error: erro => {
+                console.error('Erro ao listar carros:', erro);
+                alert('Ocorreu um erro ao carregar os carros.');
+            }
+        });
     }
 
-    
+    findByModeloLike(pesquisa: string) {
+        this.carrosService.findByModeloLike(pesquisa).subscribe({
+            next: lista => {
+                this.lista = lista;
+            },
+            error: erro => {
+                console.error('Erro na busca:', erro);
+                alert('Ocorreu um erro durante a busca.');
+            }
+        });
+    }
 }
